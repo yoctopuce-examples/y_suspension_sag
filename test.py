77766091ae6cdfe0abe691ff0e9ea6498310570c
srinsoz <nhy8KTQ!fah.bta8yjg>
2026-05-18 00:01:04 +0200

    Work in progress

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ Platformer Game
 python -m arcade.examples.platform_tutorial.02_draw_sprites
 """
 import arcade
-from yoctopuce import yocto_api
+import arcade.gui
 from yoctopuce.yocto_api import YRefParam, YAPI
 from yoctopuce.yocto_rangefinder import YRangeFinder
 
@@ -39,7 +39,7 @@ class SuspStatus:
         if sag != self.sag or mm_sag != self.mm_sag:
             self.sag = sag
             self.mm_sag = mm_sag
-            #print("update value :"+self.get_sag_text())
+            # print("update value :"+self.get_sag_text())
 
     def get_sag_text(self):
         return "SAG = %d%% (%dmm)" % (self.sag, self.mm_sag)
@@ -53,6 +53,44 @@ class Status:
     def reset(self):
         self.front.reset()
         self.rear.reset()
+
+
+class ApplicationConfig:
+    def __init__(self):
+        self.url = "usb"
+        self.error = None
+        self.all_rf = []
+        self.front_rf = None
+        self.rear_rf = None
+
+    def setup(self):
+        errmsg = YRefParam()
+        if YAPI.RegisterHub(self.url, errmsg) != YAPI.SUCCESS:
+            print("Unable to register hub %s: %s" % (self.url, errmsg.value))
+            # if usb failed it's probably because a VirtualHub is running. Try to use the VirtualHub
+            self.url = "127.0.0.1"
+            if YAPI.RegisterHub(self.url, errmsg) != YAPI.SUCCESS:
+                print("Unable to register hub %s: %s" % (self.url, errmsg.value))
+                self.error = errmsg.value
+                return
+
+        rf = YRangeFinder.FirstRangeFinder()
+        while rf is not None:
+            self.all_rf.append(rf.get_hardwareId())
+            name = rf.get_logicalName()
+            if name == "front":
+                self.front_rf = rf.get_hardwareId()
+            elif name == "rear":
+                self.rear_rf = rf.get_hardwareId()
+            rf = rf.nextRangeFinder()
+
+    def check_config(self):
+        if self.error is not None:
+            return self.error
+        if len(self.all_rf) == 0:
+            return "No Yocto-RangeFinder detected on " + self.url
+
+        return None
 
 
 class GameView(arcade.Window):
@@ -116,7 +154,6 @@ class GameView(arcade.Window):
         YAPI.HandleEvents()
         self.front_sag_text.text = self.status.front.get_sag_text()
 
-
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
         pass
@@ -135,32 +172,80 @@ def valueCallback(rf, value):
     p.update_value(int(value))
 
 
-def main():
-    """Main function"""
-    url = "usb"
-    errmsg = YRefParam()
-    print("connecting to %s" % url)
-    if YAPI.RegisterHub(url, errmsg) != YAPI.SUCCESS:
-        print("USB error: " + errmsg.value)
-        return
-    print("connected")
-    target = "front"
-    rf = YRangeFinder.FindRangeFinder(target + ".rangeFinder1")
-    if not rf.isOnline():
-        print(f"{target} not connected")
-        return
-    print("use sensor %s" % rf.get_serialNumber())
-    # Use high-accuracy mode for better precision
-    rf.set_rangeFinderMode(YRangeFinder.RANGEFINDERMODE_HIGH_ACCURACY)
-
-    # Build the parameter object, passing the GUI reference so callbacks can update it
-    status = Status(rf.get_hardwareId(), 160, "inv", 65)
-    rf.set_userData(status.front)
-    rf.registerValueCallback(valueCallback)
-    #YAPI.Sleep(5000)
-
+def start_yoctopuce_lib(config):
     window = GameView(status)
     window.setup()
+    arcade.run()
+
+
+class MainView(arcade.View):
+    """ Main application class."""
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.manager = arcade.gui.UIManager()
+        widget_layout = arcade.gui.UIBoxLayout(align="center", space_between=10)
+
+        title_label = arcade.gui.UILabel(text="Yoctopuce SAG meter", align="center", font_size=20, multiline=False)
+        title_label_space = arcade.gui.UISpace(height=30, color=arcade.color.DARK_BLUE_GRAY)
+        widget_layout.add(title_label)
+        widget_layout.add(title_label_space)
+
+        error = config.check_config()
+        if error is not None:
+            error_label = arcade.gui.UILabel(text="Error: " + error, align="center", font_size=15,
+                                             multiline=False)
+            widget_layout.add(error_label)
+            widget_layout.add(title_label_space)
+
+            exit_button = arcade.gui.UIFlatButton(text="Exit", width=150)
+            @exit_button.event("on_click")
+            def on_click_exit_button(event):
+                arcade.exit()
+            widget_layout.add(exit_button)
+        else:
+            # todo continue next
+            continue_button = arcade.gui.UIFlatButton(text="Continue", width=150)
+            widget_layout.add(continue_button)
+
+        self.anchor = self.manager.add(arcade.gui.UIAnchorLayout())
+        self.anchor.add(
+            anchor_x="center_x",
+            anchor_y="center_y",
+            child=widget_layout)
+        version_label = arcade.gui.UILabel(text="ylib : " + YAPI.GetAPIVersion(), align="center", font_size=12,
+                                           multiline=False)
+        self.anchor.add(
+            anchor_x="right",
+            anchor_y="bottom",
+            child=version_label)
+
+    def on_show_view(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        # Enable the UIManager when the view is showm.
+        self.manager.enable()
+
+    def on_hide_view(self):
+        # Disable the UIManager when the view is hidden.
+        self.manager.disable()
+
+    def on_draw(self):
+        """ Render the screen. """
+        # Clear the screen
+        self.clear()
+        # Draw the manager.
+        self.manager.draw()
+
+
+def main():
+    config = ApplicationConfig()
+    config.setup()
+
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, resizable=True)
+    main_view = MainView(config)
+    window.show_view(main_view)
     arcade.run()
 
 
